8bd1713f5d01181a8a7f5e98c8acead59929fb7d
Anatoly Anatoly <lptolik@gmail.com>
2022-05-16 17:15:55 +0100

    clusterCons robustness

--- R/memrob.R
+++ R/memrob.R
@@ -61,3 +61,37 @@ memrob <- function(x,rm=data.frame()){
 	return(mem_rob_list)
 }
 
+#' Calculate cluster robustness from consensus matrix
+#'
+#' @param gg igroph object
+#' @param alg clustering algorithm
+#' @param conmat consensus matrix
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getRobustness<-function(gg,alg,conmat){
+
+  rm<-data.frame(cm=as.numeric(igraph::get.vertex.attribute(gg,alg,V(gg))))
+  cm           <- data.frame(conmat);
+  names(cm)    <- rownames(rm);
+  rownames(cm) <- rownames(rm);
+  cm           <- as.matrix(cm);
+  kk     = max(as.numeric(as.vector(rm$cm)));
+  ##--- make the consensus matrix object for clusterCons so you can use its functions
+  out <- new('consmatrix', cm=cm, rm=rm, k=kk, a=alg);
+
+  ##--- get cluster robustness values from clusterCons
+  cr <- clusterCons::clrob(out);
+
+  ##--- the scaled cluster robustness values
+  crScales <- cr$rob
+  crScales <- (crScales-min(crScales))/(max(crScales)-min(crScales))
+  oo<-data.frame(alg=as.character(rep(alg,length(rownames(cr)))),
+             C=as.numeric(rownames(cr)),
+             Cn=as.numeric(table(rm[,1])),
+             Crob=as.numeric(cr$rob),
+             CrobScaled=as.numeric(crScales))
+
+}
