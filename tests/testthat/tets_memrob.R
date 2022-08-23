60106c1b0c8d5041d6fcfc41425ef01fb74051d8
Anatoly Sorokin <lptolik@gmail.com>
2022-08-23 17:05:55 +0900

    reformatting

--- tests/testthat/tets_memrob.R
+++ tests/testthat/tets_memrob.R
@@ -1,6 +1,7 @@
 library(AnNet)
 library(testthat)
 data(karate,package='igraphdata')
+alg<-'louvain'
 #context("Testing robustness calculations")
 test_that("proper algorithm selected",{
   expect_error(getRobustness(karate,'alg',matrix(0,ncol = 1,nrow = 1)),"Membership.*")
